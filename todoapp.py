08dd8a9f1961cd65a5c1152f8587d9eac536e4a5
Rommel Lasso <soccerply@msn.com>
2017-12-18 18:48:29 -0500

    adding directories

--- todoapp.py
+++ todoapp.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Assignemnt Week 11 - Flask App"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
