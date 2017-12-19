adddbb14c3fe0887db028aa90e5fe96d93e8a895
Rommel Lasso <soccerply@msn.com>
2017-12-18 21:14:24 -0500

    Addded HTML Component

--- todoapp.py
+++ todoapp.py
@@ -2,12 +2,17 @@
 # -*- coding: utf-8 -*-
 """Assignemnt Week 11 - Flask App"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
+list_data = {
+    '1': ['Get Run in','rlasso@aol.com.com','high'],
+    '2': ['Make Bfas', 'class@aol.com','low']
+}
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html', list_data=list_data)
 
 if __name__ == '__main__':
     app.run()
